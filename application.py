f8477a0f918fe6ba33d0eda9bff0741af07237bc
Udbhav Sharma <udbhav.n.sharma.ct@gmail.com>
2021-07-23 17:10:58 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,12 +4,12 @@ from PIL import Image
 pytesseract.pytesseract.tesseract_cmd= '/app/.apt/usr/bin/tesseract'
 
 st.title("IMAGE TO TEXT CONVERTER")
-i= st.sidebar.file_uploader("Choose an image:")  #'sidebar' moves our browzer to the left side of the website page.
+i= st.sidebar.file_uploader("Choose an image:")
 
-if i is not None:    #Need to write 'if' since streamlit works in loops and until it receives the image we have uploaded, it will show an error saying no image found.
-  st.image(i, caption= 'YOUR IMAGE') #displaying image.
-  img= Image.open(i) #reading image so as to be able to use it.
-  if st.button('DETECT'):    #this if creates the button 'DETECT' and shows the required output only if that button is pressed.
+if i is not None:
+  st.image(i, caption= 'YOUR IMAGE')
+  img= Image.open(i)
+  if st.button('DETECT'):
     op= pytesseract.image_to_string(img)
     st.text("THE CONVERTED TEXT IS: ")
     st.write(op)
